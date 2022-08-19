3edc20ad5a23b885cb1432bc76d0732c29a26845
as <staudtlex@live.de>
2022-08-19 19:33:45 +0200

    Rename minimal_json.go to json.go

--- json.go
+++ json.go
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-// This file implements a basic JSON parser sufficient to unmarshal
+// This file implements a minimal JSON parser sufficient to unmarshal
 // JSON-serialized Date objects
 
 package libcalendar
